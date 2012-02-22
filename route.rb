13d68717db1fbadd4a67cd5d6ddb22245f05d065
Douglas Calhoun <douglas.calhoun@gmail.com>
2012-02-22 15:34:37 -0800

    created add method on Route class

--- route.rb
+++ route.rb
@@ -14,22 +14,28 @@ class Route
     @time_in_seconds = time_in_seconds
   end
   
-  def compare(route)
+  def compare(other_route)
     
-    if @origin != route.origin || @destination != route.destination
+    if @origin != other_route.origin || @destination != other_route.destination
       return "These routes are inherently different. You can't compare apples and oranges!"
     end
     
     my_weighted_value = (@cost_in_pennies*100)+(@time_in_seconds*10)
-    other_route_weighted_value = (route.cost_in_pennies*100)+(route.time_in_seconds*10)
+    other_route_weighted_value = (other_route.cost_in_pennies*100)+(other_route.time_in_seconds*10)
     
     if my_weighted_value > other_route_weighted_value
-      route
+      other_route
     elsif my_weighted_value < other_route_weighted_value
       self
     else
       "These routes are pretty much identical."
     end
     
+  end
+  
+  def add(other_route)
+    total_cost = @cost_in_pennies + other_route.cost_in_pennies
+    total_time = @time_in_seconds + other_route.time_in_seconds
+    Route.new(origin, other_route.destination, total_cost, total_time)
   end
 end
