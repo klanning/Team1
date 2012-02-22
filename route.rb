5be06b55264b87a72376b1c9320aa9cf3099c1ab
Ryan Coppolo and David J. Phillips <david.ryan.dbc@gmail.com>
2012-02-21 17:40:08 -0800

    Adds some tests for route comparison

--- route.rb
+++ route.rb
@@ -9,4 +9,24 @@ class Route
     @time_in_seconds = time_in_seconds
   end
   
+  def compare(route)
+    if @origin != route.origin || @destination != route.destination
+      return "These routes are inherently different. You can't compare apples and oranges!"
+    end
+    @time_in_seconds 
+    route.cost_in_pennies 
+    route.time_in_seconds
+    
+    my_weighted_value = (@cost_in_pennies*100)+(@time_in_seconds*10)
+    other_route_weighted_value = (route.cost_in_pennies*100)+(route.time_in_seconds*10)
+    
+    if my_weighted_value == other_route_weighted_value
+      "These routes are pretty much identical."
+    elsif
+      my_weighted_value > other_route_weighted_value
+      "You should take the Driving Route because it is faster and cheaper!"
+    else
+      "You should take the Bart Route because it is faster and cheaper!"
+    end
+  end
 end
--- route_rspec.rb
+++ route_rspec.rb
@@ -5,7 +5,23 @@ describe Route do
   before(:each) do
     origin = "923 Market St. SF, CA"
     destination = "100 California St. SF, CA"
-    @bart_route = Route.bart(origin, destination)
+    destination2 = "A galaxy far far away..."
+    
+    
+    @route1 = Route.new(origin,destination,150,100)
+    @route2 = Route.new(origin,destination,150,100)
+    @route3 = Route.new(origin,destination2,100,100)
+  end
+  
+  describe "#compare" do
+    it "should find Routes with equal time and cost equal " do
+      @route1.compare(@route2).should == "These routes are pretty much identical."
+    end
+    
+    it "shouldn't be able to compare routes to different places" do
+      @route3.compare(@route2).should == "These routes are inherently different. You can't compare apples and oranges!"
+    end
+  
   end
       
 end
