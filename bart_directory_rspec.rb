0bc0970affa1c5aee0827317e51d9d0c51367276
Ryan Coppolo and David J. Phillips <david.ryan.dbc@gmail.com>
2012-02-22 16:20:14 -0800

    Updated bart directory to find the closest bart station

--- bart_directory_rspec.rb
+++ bart_directory_rspec.rb
@@ -8,20 +8,25 @@ describe BartDirectory do
   
   describe '#bart_station?' do
     it "should check if an address is a Bart station" do
-      origin = "2800 Mission Street"
+      origin = "2000 Mission Street"
       destination = "Sunset Blvd."
-      @directory.bart_address?(origin).should == true
-      @directory.bart_address?(destination).should == false
+      @directory.bart_address?(origin).should == origin
+      @directory.bart_address?(destination).should == nil
     end
   end
   
   describe "#closest_station" do
-    it "should return the closest station"
+    it "should return the closest station" do
+      origin = "2100 Mission Street San Francisco CA"
+      destination = "2800 Diamond Street San Francisco CA"
+      @directory.closest_station(origin).should == "2000 Mission Street"
+      @directory.closest_station(destination).should == "2901 Diamond Street"
+    end
   end
   
   describe '#get_abbr' do
     it "should return the abbrevation of a bart station given an address" do
-      @directory.get_abbr("2800 Mission Street").should == "24TH"
+      @directory.get_abbr("2000 Mission Street").should == "16TH"
     end
   end
   
